b437fc21a6abc07e6b0c866a95710d378654c25b
Louis Dupla <louisdupla@macbook-pro-de-louis-1.home>
2025-12-18 18:24:52 +0100

    fix mail

--- web/src/pages/AuthCallbackPage.tsx
+++ web/src/pages/AuthCallbackPage.tsx
@@ -2,14 +2,7 @@ import { useEffect } from "react";
 
 export default function AuthCallbackPage() {
   useEffect(() => {
-    // Si tu utilises HashRouter, tes routes sont sous "#/..."
-    const usesHashRouter = window.location.hash.startsWith("#/");
-
-    if (usesHashRouter) {
-      window.location.replace("/#/signin?confirmed=1");
-    } else {
-      window.location.replace("/signin?confirmed=1");
-    }
+    window.location.replace("/signin?confirmed=1");
   }, []);
 
   return <div>Email confirmé ✅ Redirection…</div>;
